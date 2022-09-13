36206cbd89ed386f77f523ad166de0c3d875a66e
Agustin Jacobsen <agustin.jacobsen@mercadolibre.com>
2022-09-13 15:45:23 -0300

    Revert "Feature/comunication with ip api"

--- src/common/Request.dto.ts
+++
@@ -1,3 +0,0 @@
-export interface RequestPayloadDTO {
-    ip: string
-}
--- src/services/repositories/domain/country.ts
+++
@@ -1,5 +0,0 @@
-export interface Country {
-    uuid: string,
-    longest_distance: number,
-    requests_amount: number,
-}
