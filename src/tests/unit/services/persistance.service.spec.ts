dd2873e31ac34e4d049c5d49eddfaf06ae1fa88e
Agustin Jacobsen <jacobsen816@gmail.com>
2022-09-14 14:12:30 -0300

    fixed tests

--- src/tests/unit/services/persistance.service.spec.ts
+++ src/tests/unit/services/persistance.service.spec.ts
@@ -1,7 +1,6 @@
 import { PersistanceService } from '../../../services/persistance.service';
 import { CountryRepository } from '../../../services/repositories/impl/mock/country.repository';
 import { Country } from '../../../services/repositories/domain/country';
-import { SingletonDB } from '../../../common/db.mock';
 
 const repository = new CountryRepository();
 const persistanceServiceMock = new PersistanceService(repository);
@@ -9,37 +8,6 @@ describe('Persistance service test', () => {
     process.env.SCHEMA = "countries-test";
     jest.setTimeout(9000000);
 
-    beforeAll(() => {
-        const deleteParams = {
-            TableName: 'countries-dev'
-        };
-
-        SingletonDB.getInstance().ddb.deleteTable(deleteParams, function (err, data) {
-            if (err) {
-                console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2));
-            }
-        });
-
-
-        const createParams = {
-            TableName: `countries-test`,
-            KeySchema: [{ AttributeName: "code", KeyType: "HASH" }],
-            AttributeDefinitions: [
-                { AttributeName: "code", AttributeType: "S" }
-            ],
-            ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-        };
-
-        SingletonDB.getInstance().ddb.createTable(createParams, function (err, data) {
-            if (err) {
-                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("create table. Table description JSON:", JSON.stringify(data, null, 2));
-            }
-        });
-    });
     test('updateStatistics - save new country and would be the most requested', async () => {
         const result = await persistanceServiceMock.updateStatistics('AR', 123, 'Argentina');
         const resultMostRequested = await persistanceServiceMock.getMostRequestedCountry();
